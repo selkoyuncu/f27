86961fca2adc3c6f0c8c4c5cbb43d3ac951a0041
selkoyuncu <82313309+selkoyuncu@users.noreply.github.com>
2023-09-09 00:13:48 +0300

    .

--- test/TestFundMe.t.sol
+++ test/TestFundMe.t.sol
@@ -2,18 +2,21 @@
 pragma solidity ^0.8.18;
 
 import {Test} from "../lib/forge-std/src/Test.sol";
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {FundMe} from  "../src/FundMe.sol";
 
 
 contract TestFundMe is Test {
-    
 
+    FundMe private fundMeObj ;
 
     function setUp() external {
-
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMeObj = deployFundMe.run();
     }
 
     function test_fund() external {
-        
+        fundMeObj.fundMe();
     }
 
 }
